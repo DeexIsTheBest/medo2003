034158055bafb7a157cf2271e0cc700ac8cf1159
DeexIsTheBest <45711286+DeexIsTheBest@users.noreply.github.com>
2018-12-19 18:46:44 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setActivity("-Moments Music",{type: 'WATCHING'})
+   client.user.setActivity(", Vortex Music",{type: 'WATCHING'})
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
